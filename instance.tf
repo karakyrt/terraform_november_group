8973e266ae0b7357e3449c4864b9ea4b411a3db9
karakyrt <44345658+karakyrt@users.noreply.github.com>
2019-02-27 21:39:12 -0600

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,4 +1,13 @@
-resource "aws_instance" "example" {
-  instance_type = "t2.micro"
-  ami = "ami-0fad7378adf284ce0"
+resource "aws_instance"  "example"{
+  instance_type                =  "${var.instance_type}"
+  ami                          =  "ami-0fad7378adf284ce0"
+  key_name                     = "${var.key_name}"
+  subnet_id                    = "${aws_subnet.dev1.id}"
+  associate_public_ip_address  =  "true"
+  count  =  "${var.count}"
+
+ tags {
+   Environment = "${var.Environment}${count.index +1}"
+   Create_by   =  "${var.Created_by}"
+  }
 }
